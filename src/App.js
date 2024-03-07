21d4121b470860bec06a5d341ab3969626fb35b8
tim <timmcsweeney12@hotmail.com>
2024-03-07 21:34:28 +0000

    Users are now able to put in there own custom time for the timer. Also added a input box where the user has the option to input seconds aswell.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,57 @@ import './App.css';
 import React, {useEffect, useState} from 'react';
 import image1 from "./image/pic2.jpg"
 import image2 from "./image/picWide.jpg"
+import image3 from "./image/pic3.jpg"
+import image4 from "./image/pic4.jpg"
 
-const backGrounds = [image1,image2];
+
+const backGrounds = [image1,image2,image3,image4];
 
 
 
 function App() {
   const [currentBackground,setCurrentBackground] = useState(0);
-  const [time,setTime] = useState(1500000);
+  const [time,setTime] = useState(0);
   const [timerOn,setTimeOn] = useState(false); 
-  const [userNumber,setUserNumber] = useState(' ');
+  const [minutes,setMinutes] = useState(0);
+  const [seconds,setSeconds] = useState(0);
+
+
+  function changeBackground(){
+
+    //using this function to cycle through the differant backgrounds
+    const nextBackgroundIndex = (currentBackground + 1) % backGrounds.length;
+    setCurrentBackground(nextBackgroundIndex);
+       
+      }
+
+      function handleMinuteChange(e){
+        setMinutes(parseInt(e.target.value,10)||0);
+       
+      }
+
+      function handleSecondsChange(e){
+        setSeconds(parseInt(e.target.value,10)||0); 
+        
+      }
+
+       
+      
+
+
+      useEffect(function updateTimer() {
+
+        const millisecs = (minutes * 60000) + (seconds * 1000);
+        setTime(millisecs);
+      }, [minutes,seconds])
+
+
+
+  
 
   useEffect(() =>{
+
+    // timer
     let interval = null;
 
     if(timerOn){
@@ -28,15 +67,6 @@ function App() {
 
     return () => clearInterval(interval)
   },[timerOn])
-  
-
-  function changeBackground(){
-
-const nextBackgroundIndex = (currentBackground + 1) % backGrounds.length;
-setCurrentBackground(nextBackgroundIndex);
-   
-  }
-
   
 
   return (
@@ -59,6 +89,9 @@ setCurrentBackground(nextBackgroundIndex);
           <button onClick={() => setTimeOn(false)}>Stop</button>
           <button onClick={() => setTimeOn(true)}>Resume</button>
           <button onClick={() => setTime(0)}>Reset</button>
+          <input type="number" onChange={handleMinuteChange} placeholder='Minutes' ></input>
+          <input type="number" onChange={handleSecondsChange} placeholder='Seconds'  ></input>
+          <h1>{time}</h1>
         </div>
       </div>
     </section>
