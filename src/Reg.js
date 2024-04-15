a9dbd4de0ef83b929815941e569cdfbaba59bb74
tim <timmcsweeney12@hotmail.com>
2024-04-15 16:16:48 +0100

    bootstrap styling import

--- src/Reg.js
+++ src/Reg.js
@@ -1,13 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import { auth } from "./firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
+// import { auth } from "./firebase";
+// import { useAuthState } from "react-firebase-hooks/auth";
 // import 'bootstrap/dist/scc/bootstrap.css';
-// import 'bootstrap/dist/scc/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 //import { addData } from './firestoreService';
 
 
 function Reg() {
-    const [user] = useAuthState(auth);
+    // const [user] = useAuthState(auth);
     const [userName, setUserName] = useState(0);
 
     function onUserNameChange(event) {
@@ -21,8 +22,8 @@ function Reg() {
     return (
         <section>
             User Name:
-            <input onChange={onUserNameChange} type="text"/>
-            <button onClick={Submit} className='btn'>Submit</button>
+            <input classonChange={onUserNameChange} type="text"/>
+            <button onClick={Submit} className='btn btn-secondary'>Submit</button>
         </section>
     )
 }
