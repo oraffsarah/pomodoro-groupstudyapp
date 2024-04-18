3cacd888aef9708c163697a08b1165d3a3c4a3cc
tim <timmcsweeney12@hotmail.com>
2024-04-18 19:47:29 +0100

    removed uneeded

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 
 import './App.css';
 import React, {useEffect, useState} from 'react';
-import GoogleButton from 'react-google-button'
+
 
 import Timer from './components/Timer';
-import SignIn from './pages/Signin';
-import { Routes ,Route } from 'react-router-dom';
-import { AuthContextProvider } from './context/authContext';
-import Account from './pages/Account';
-import Home from './pages/Home';
-
+=
 
 
 
